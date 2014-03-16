2ec1e2a249591a0be311dc3e2807809f847c18f7
laakhbeenor <helena@ubuntu.(none)>
2014-03-16 22:54:11 +0100

    Winner is coming

--- Play/Trick.cpp
+++ Play/Trick.cpp
@@ -11,6 +11,17 @@ bool Trick::add(const Player &, const Card &){
 	cards.push_back(playerAndCard);
 }
 Player const& Trick::getWinner(){
-	return cards.front();
+
+	PlayerAndCard actualWinner;
+	actualWinner.card=cards.front().card; 
+	actualWinner.player=cards.front().player;
+
+	for(vector<PlayerAndCard>::iterator it=cards.begin()+1;it!=cards.end();it++){
+		if((it->card.suit==denomination && actualWinner.card.suit==denomination) || (it->card.suit==actualWinner.card.suit)){
+			if(actualWinner.card.rank < it->card.rank) actualWinner=(*it);	
+		}
+		if(it->card.suit==denomination && actualWinner.card.suit!=denomination) actualWinner=(*it);
+	}
+	return actualWinner;
 }
 
