35ae5b30c93e00f3cc9a3b285c64b5587cf955c2
laakhbeenor <helena@ubuntu.(none)>
2014-03-17 21:58:46 +0100

    cokolwiek

--- Play/Test/TrickTest.cpp
+++ Play/Test/TrickTest.cpp
@@ -1,13 +1,14 @@
 #include <gtest/gtest.h>
 #include "../Play.hpp"
 
-class TrickFixture : ::testing::Test{
-	Trick trick;
+class TrickFixture : public ::testing::Test{
 	Player player;
 	Card card;
+	Denomination denomination;
 };
 
 TEST_F(TrickFixture, Adding){
+	Trick trick(denomination);
 	trick.add(player, card);
 	Player const& winner = trick.getWinner();
 	ASSERT_EQ(winner, player);
